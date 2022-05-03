2034b0cf16eb983b30825e6df172058ceedfa5c4
ABaldwin1297 <abaldwin1297@gmail.com>
2022-05-03 16:26:56 -0400

    Add project files.

---
+++ ForLoopIndexing/Program.cs
@@ -0,0 +1,63 @@
+﻿// Assignment slide 98
+
+//var total = 0;
+
+//for (var idx = 1; idx <= 50; idx++) {
+//    if (idx % 5 == 0 || idx % 7 == 0) {
+//        total = total + idx;
+//    }  
+
+////}
+
+////Console.WriteLine($"Total is {total}");
+
+
+
+////string[] names = { "Ann", "Bob", "Chris", "Darlene", "Ed" };
+
+////foreach(var name in names) {
+////    Console.WriteLine(name);
+//}
+
+
+
+
+//int[] nbrs = {                                      // int[] is declaring an array of integers
+//754, 233, 509, 792, 700, 596, 833, 658, 998, 742,
+//187, 754, 308, 914, 489, 867, 717, 586, 929, 467,
+//460, 241, 770, 324, 599, 259, 120, 800, 336, 609,
+//690, 134, 598, 249, 282, 574, 334, 956, 659, 214,
+//435, 643, 809, 874, 906, 620, 328, 369, 426, 561
+//};
+
+//var high = 0;
+//var low = 1000;
+
+//foreach (var nbr in nbrs) {
+//    if (nbr > high) {
+//        high = nbr;
+//    }
+//    if (nbr < low) {
+//        low = nbr;
+//    }
+//}
+
+//    Console.WriteLine($"Highest is {high}; lowest is {low}");
+
+
+
+//for(var idx = 1; idx <=30; idx++) {
+//    if (idx % 3 == 0 && idx % 5 == 0) {
+//        Console.WriteLine($"FIZZBUZZ, ");
+//        continue;
+//    }
+//    if(idx % 3 == 0) {
+//        Console.WriteLine($"FIZZ, ");
+//        continue;
+//    }
+//    if (idx % 5 == 0) {
+//        Console.WriteLine($"BUZZ, ");
+//        continue;
+//    }
+//    Console.WriteLine($"{idx}, ");
+}
